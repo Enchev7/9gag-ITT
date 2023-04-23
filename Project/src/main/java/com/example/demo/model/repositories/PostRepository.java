c4f299a3826a5cf27f04a0c3d3262cb1bba0da85
Sokol Tsvetanov <sokolcvetanov@gmail.com>
2023-04-23 14:29:20 +0300

    Fixed trending

--- Project/src/main/java/com/example/demo/model/repositories/PostRepository.java
+++ Project/src/main/java/com/example/demo/model/repositories/PostRepository.java
@@ -20,18 +20,18 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByTagNameContainingIgnoreCase(String searchString);
 
 
-    @Query(value = "SELECT p.id, p.title, p.content, p.created_at, p.reports " +
-            "(SELECT COUNT(c.id) FROM comments c WHERE c.post_id = p.id) + " +
-            "(SELECT COUNT(pr.post_id) FROM post_reactions pr WHERE pr.post_id = p.id AND pr.is_liked = 1) AS total_count " +
-            "FROM posts p WHERE p.created_at >= DATE_SUB(NOW(), INTERVAL 9 DAY) ORDER BY total_count DESC ",
+    @Query(value = "SELECT p.id, p.title, p.file_path, p.user_id, p.created_at, p.reports," +
+            "            (SELECT COUNT(c.id) FROM comments c WHERE c.post_id = p.id) + " +
+            "           (SELECT COUNT(pr.post_id) FROM post_reactions pr WHERE pr.post_id = p.id AND pr.is_liked = 1) AS total_count" +
+            "            FROM posts p ORDER BY total_count DESC",
             countQuery = "SELECT COUNT(*) FROM posts WHERE created_at >= DATE_SUB(NOW(), INTERVAL 1 DAY)",
             nativeQuery = true)
     Page<Post> sortedByTrending(Pageable pageable);
 
 
-    @Query(value = "SELECT p.id, p.title, p.file_path, p.user_id, p.created_at, p.reports, " +
-            "(SELECT COUNT(c.id) FROM comments c WHERE c.post_id = p.id) + " +
-            "(SELECT COUNT(pr.post_id) FROM post_reactions pr WHERE pr.post_id = p.id AND pr.is_liked = 1) AS total_count " +
+    @Query(value = "SELECT p.id, p.title, p.file_path, p.user_id, p.created_at, p.reports, \n" +
+            "(SELECT COUNT(c.id) FROM comments c WHERE c.post_id = p.id) + \n" +
+            "(SELECT COUNT(pr.post_id) FROM post_reactions pr WHERE pr.post_id = p.id AND pr.is_liked = 1) AS total_count\n" +
             "FROM posts p ORDER BY total_count DESC", countQuery = "SELECT COUNT(*) FROM posts", nativeQuery = true)
     Page<Post> sortedByTop(Pageable pageable);
 
